4906f473ec058d6886ba627de385ddc4343187b7
Abhirami A J <99177444+AB-hirami@users.noreply.github.com>
2022-03-06 17:54:27 +0530

    largest of 3 nos

---
+++ largest of 3 nos.py
@@ -0,0 +1,11 @@
+print("enter the 3 nos:")
+a = int(input())
+b = int(input())
+c = int(input())
+if a>b and a>c:
+        print("largest no is:", a)
+elif b>a and b>c:
+        print("largest no is:", b)
+else:
+        print("largest no is:", c)
+    
