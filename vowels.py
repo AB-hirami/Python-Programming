f1952cb35054fdcf91977ee24928598471e86434
Abhirami A J <99177444+AB-hirami@users.noreply.github.com>
2022-03-06 12:08:22 +0530

    vowels.py

---
+++ vowels.py
@@ -0,0 +1,7 @@
+words = ['apple', 'orange', 'pear', 'milk', 'otter', 'snake', 'iguana',
+         'tiger', 'eagle']
+vowel=[]
+for vowel in words:
+    if vowel [0]=='a,e':
+        words.append(vowel)
+    print (words)
